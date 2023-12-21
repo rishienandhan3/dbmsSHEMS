b0d4b1f4e953adb90c8efd7da646c995adab413e
Rishie <>
2023-12-20 21:32:10 -0500

    Added base.html file with bootstrap configured through CDN

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
+# create a Flask App instance:
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
+
+
+@app.route('/<name>/')
+def user(name):
+    return f"<h3> Hello {name}! </h3>"
+
+
+@app.route('/admin/')
+def admin():
+    return redirect(url_for('user', name='Admin!'))
+
+
+@app.route('/printname/<name>/')
+def printname(name):
+    return render_template("print.html", content=name)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
